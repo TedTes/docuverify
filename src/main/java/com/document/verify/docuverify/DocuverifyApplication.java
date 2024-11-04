1d3e6012b42b55185a2b0e36f2c19490b145db38
Tedros <tedtfu@gmail.com>
2024-11-04 09:26:06 -0500

    docuverify init

---
+++ src/main/java/com/document/verify/docuverify/DocuverifyApplication.java
@@ -0,0 +1,13 @@
+package com.document.verify.docuverify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocuverifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocuverifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/document/verify/docuverify/DocuverifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.document.verify.docuverify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocuverifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
