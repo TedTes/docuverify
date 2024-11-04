6be6ceb41bd79816c9f0ae755dd80ca95f42b1c5
Tedros <tedtfu@gmail.com>
2024-11-04 10:19:27 -0500

    + documentService.java

---
+++ src/main/java/com/document/verify/docuverify/DocumentService.java
@@ -0,0 +1,20 @@
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class DocumentService {
+    private MerkelTree merkelTree;
+    private List<String> documents;
+
+    public String initializeTree(List<String> documentData) {
+        this.documents = documentData;
+        this.merkelTree = new MerkelTree(documents);
+        return merkelTree.getRootHash();
+    }
+    public List<String> getProofForDocument(int index) {
+        return merkelTree.getProof(index);
+    }
+    public boolean verifyDocument(String document , List<String> proof, String rootHash) {
+        return DocumentVerifier.verify(document, rootHash,proof);
+    }
+}
